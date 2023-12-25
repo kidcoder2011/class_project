43b76d0d8796d93560fadf96e08c8938945af06f
kidcoder2011 <154789768+kidcoder2011@users.noreply.github.com>
2023-12-25 20:47:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function search(){
+    var serValue = document.getElementById("serbox").value;
+    var names = ['vishal',"sriram","rishikesh","bhuvan","kaja","S.J","daniel","D.K","sasendra","saaketh","gnana sai","blesson","harshavardhan","ujwal","swaran","bhargav","abhinash" ];
+    if(names.includes(serValue)){
+        window.location.href = 'https://kidcoder2011.github.io/class_project/home.html';
+    }
+}
